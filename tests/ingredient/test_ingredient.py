f1912b7821062768bfc4b67bb747c6e14e889f02
Gabriela Muniz <gabrielanamu@live.com>
2023-06-19 14:54:54 -0300

    feat: add req01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
+from src.models.ingredient import Restriction # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("ovo")
+    ingredient2 = Ingredient("manteiga")
+
+    assert ingredient1.__hash__() != ingredient2.__hash__()
+    assert ingredient1.__hash__() == ingredient1.__hash__()
+
+    assert ingredient1.__eq__(ingredient2) is False
+    assert ingredient1.__eq__(ingredient1) is True
+
+    assert ingredient1.__repr__() == "Ingredient('ovo')"
+
+    assert ingredient1.name == "ovo"
+    assert ingredient2.name == "manteiga"
+
+    assert ingredient1.restrictions == {Restriction.ANIMAL_DERIVED}
